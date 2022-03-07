d41268b5009353cf33dbd533c9eb0028122c360c
최자령 <pauljr@intube.kr>
2022-03-07 16:09:47 +0900

    Add jpa example

---
+++ src/main/java/org/example/blog/model/Board.java
@@ -0,0 +1,4 @@
+package org.example.blog.model;
+
+public class Board {
+}
---
+++ src/main/java/org/example/blog/model/User.java
@@ -0,0 +1,31 @@
+package org.example.blog.model;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+// ORM -> Java(다른언어 포함) Object -> 테이블로 매핑해주는 기술
+@Entity // User 클래스가 MYSQL 에 테이블로 생성이 된다.
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 프로젝트에서 연결된 DB의 넘버링 전략을 따라간다.
+    private int id;
+
+    @Column(nullable = false, length = 30)
+    private String username;
+
+    @Column(nullable = false, length = 100)
+    private String password;
+
+    @Column(nullable = false, length = 50)
+    private String email;
+
+    @ColumnDefault("'user'") // 문자라는 것을 알려줘야 한다.
+    private String role; // Enum 을 쓰는 것이 좋다. // admin, user, manager
+
+    @CreationTimestamp // 시간 자동입력
+    private Timestamp createDate;
+}
