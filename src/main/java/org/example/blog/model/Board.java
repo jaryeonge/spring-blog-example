9e486dba628a6b4c7d2e6dbdfc2565f3d74df817
최자령 <pauljr@intube.kr>
2022-03-07 16:43:07 +0900

    Add Board table

--- src/main/java/org/example/blog/model/Board.java
+++ src/main/java/org/example/blog/model/Board.java
@@ -1,4 +1,31 @@
 package org.example.blog.model;
 
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
 public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Lob // 대용량 데이터
+    private String content; // 섬머노트 라이브러리 <html> 태그가 섞여서 디자인이 됨.
+
+    @ColumnDefault("0")
+    private int count;
+
+    @ManyToOne // Many = Board, One = User
+    @JoinColumn(name="userId")
+    private User user; // DB는 오브젝트를 저장할 수 없다. FK, 자바는 오브젝트를 저장할 수 있다.
+
+    @CreationTimestamp
+    private Timestamp createDate;
 }
