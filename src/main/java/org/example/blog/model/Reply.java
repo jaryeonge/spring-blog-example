514c8171d86ed06c8dc1cda05ff74d12561de152
최자령 <pauljr@intube.kr>
2022-03-07 16:49:41 +0900

    Add Reply table

--- src/main/java/org/example/blog/model/Reply.java
+++ src/main/java/org/example/blog/model/Reply.java
@@ -1,4 +1,35 @@
 package org.example.blog.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Reply {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false, length = 200)
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "boardId")
+    private Board board;
+
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    private User user;
+
+    @CreationTimestamp
+    private Timestamp createDate;
 }
