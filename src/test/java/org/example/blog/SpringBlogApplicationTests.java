99a753f7e29926ccbc5b4b5a5d9fd3dbb8835a72
최자령 <pauljr@intube.kr>
2022-03-07 12:23:15 +0900

    Initial Commit

---
+++ src/test/java/org/example/blog/SpringBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+}
