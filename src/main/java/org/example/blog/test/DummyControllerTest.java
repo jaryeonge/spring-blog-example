e7f786b9d40134f944e2fa0bcac8ae73bbffc3aa
최자령 <pauljr@intube.kr>
2022-03-07 17:44:38 +0900

    Add dummy test(select user)

--- src/main/java/org/example/blog/test/DummyControllerTest.java
+++ src/main/java/org/example/blog/test/DummyControllerTest.java
@@ -4,9 +4,9 @@ import org.example.blog.model.RoleType;
 import org.example.blog.model.User;
 import org.example.blog.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.function.Supplier;
 
 @RestController
 public class DummyControllerTest {
@@ -26,5 +26,33 @@ public class DummyControllerTest {
         user.setRole(RoleType.USER);
         userRepository.save(user);
         return "회원가입이 완료되었습니다.";
+    }
+
+    @GetMapping("/dummy/user/{id}")
+    public User detail(@PathVariable int id) {
+        // Optional => User or Null
+//        User user = userRepository.findById(id).get();
+//        User user = userRepository.findById(id).orElseGet(new Supplier<User>() {
+//            @Override
+//            public User get() {
+//                return new User();
+//            }
+//        });
+
+//        User user = userRepository.findById(id).orElseThrow(new Supplier<IllegalArgumentException>() {
+//            @Override
+//            public IllegalArgumentException get() {
+//                return new IllegalArgumentException("해당 유저는 없습니다. id: " + id);
+//            }
+//        });
+
+        // 요청: 웹브라우저
+        // user 객체 = 자바 오브젝트
+        // 변환 (웹브라우저가 이해할 수 있는 데이터) -> json (Gson 라이브러리)
+        // 스프링부트 = MessageConverter 응답시에 자동 작동
+        // 만약에 자바 오브젝트를 리턴하게 되면 MessageConverter 가 Jackson 라이브러리를 호출해서
+        // user 오브젝트를 json 으로 변환해서 브라우저에게 던저 준다.
+        // lambda
+        return userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 유저는 없습니다. id: " + id));
     }
 }
