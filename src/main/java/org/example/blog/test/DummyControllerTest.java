4a1ef65df086688c8f559174284c2d864fdbf8b7
최자령 <pauljr@intube.kr>
2022-03-08 10:27:54 +0900

    Add dummy test(update user)

--- src/main/java/org/example/blog/test/DummyControllerTest.java
+++ src/main/java/org/example/blog/test/DummyControllerTest.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,6 +18,26 @@ public class DummyControllerTest {
 
     @Autowired // 의존성 주입
     private UserRepository userRepository;
+
+    // dirty checking
+    // 1차 캐시에 있는 객체의 변경을 감지해서 자동 DB 업데이트
+    @Transactional // 함수 종료시에 자동 commit 이 된다.
+    @PutMapping("/dummy/user/{id}")
+    public User updateUser(@PathVariable int id, @RequestBody User requestUser) {
+        System.out.println("id: " + requestUser.getId());
+        System.out.println("password: " + requestUser.getPassword());
+        System.out.println("email: " + requestUser.getEmail());
+
+        User user = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("수정에 실패하였습니다."));
+        user.setPassword(requestUser.getPassword());
+        user.setEmail(requestUser.getEmail());
+
+        // save 함수는 id 를 전달하지 않으면 insert 를 해주고
+        // save 함수는 id 를 전달하면 해당 id 에 대한 데이터가 있으면 update 를 해주고
+        // save 함수는 id 를 전달하면 해당 id 에 대한 데이터가 없으면 insert 를 한다.
+        // userRepository.save(user);
+        return null;
+    }
 
     @PostMapping("/dummy/join")
     public String join(@RequestBody User user) {
