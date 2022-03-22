d10d8bf052d3abf74356ab3a5fe357026eaa49ce
최자령 <pauljr@intube.kr>
2022-03-22 14:53:31 +0900

    Change url address

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -18,13 +18,13 @@ let index = {
         // ajax 호출시 default 비동기 호출
         $.ajax({
             type: "POST",
-            url: "/blog/api/user",
+            url: "/api/user",
             data: JSON.stringify(data),
             contentType: "application/json; charset=utf-8",
             dataType: "json"
         }).done(function(response){
             alert("회원가입이 완료되었습니다.");
-            location.href = "/blog";
+            location.href = "/";
         }).fail(function(error){
             alert(JSON.stringify(error));
         });
@@ -39,13 +39,13 @@ let index = {
         // ajax 호출시 default 비동기 호출
         $.ajax({
             type: "POST",
-            url: "/blog/api/user/login",
+            url: "/api/user/login",
             data: JSON.stringify(data),
             contentType: "application/json; charset=utf-8",
             dataType: "json"
         }).done(function(response){
             alert("로그인이 완료되었습니다.");
-            location.href = "/blog";
+            location.href = "/";
         }).fail(function(error){
             alert(JSON.stringify(error));
         });
