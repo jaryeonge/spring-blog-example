d4604eac2cb312a9797fa429c56af7b72cbd2d24
최자령 <pauljr@intube.kr>
2022-04-06 10:00:06 +0900

    Add spring security login

--- src/main/java/org/example/blog/controller/BoardController.java
+++ src/main/java/org/example/blog/controller/BoardController.java
@@ -1,5 +1,7 @@
 package org.example.blog.controller;
 
+import org.example.blog.config.auth.PrincipalDetail;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -7,8 +9,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class BoardController {
 
     @GetMapping({"","/"})
-    public String index() {
+    public String index(@AuthenticationPrincipal PrincipalDetail principal) { // 컨트롤러에서 세션을 어떻게 찾는지?
         // /WEB-INF/views/index.jsp
+        System.out.println("로그인 사용자 아이디: " + principal.getUsername());
         return "index";
     }
 }
--- src/main/java/org/example/blog/repository/UserRepository.java
+++ src/main/java/org/example/blog/repository/UserRepository.java
@@ -4,11 +4,13 @@ import org.example.blog.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.Optional;
+
 // DAO
 // 자동으로 bean 등록이 된다.
 // @Repository 생략 가능하다.
 public interface UserRepository extends JpaRepository<User, Integer> {
-
+    Optional<User> findByUsername(String username);
 }
 
 // JPA Naming Query
